0ca07ac93be29f79d5f58cb6b3b7d5f49340b1d8
Carlos <gotgoodjob@gmail.com>
2025-11-02 08:27:08 +0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Carlos Antonio dos Santos on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
